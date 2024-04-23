c9642ac58e76078a6b0b6bf3f79fcc56e68fa9c6
robnez44 <00044623@uca.edu.sv>
2024-04-22 22:59:12 -0600

    criba de eratostenes v.20 (ya funciona)

--- Algoritmos/CribaDeEratostenes.cpp
+++ Algoritmos/CribaDeEratostenes.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main(){
-
+    
     int n;
+    vector<bool> isPrime;
+    vector<int> primes;
 
     cin >> n;
 
-    for (int i = 2 ; i < n; i++)
-    { 
-        if ((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0))
-        {
-            cout << i << endl;
+    isPrime = vector<bool>(n, true);
+    primes = vector<int>();
+    
+    isPrime[0] = isPrime[1] = false;
+    
+    for (int i=2; i<n; ++i) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+            for (int h=2; h*i<n; ++h) {
+                isPrime[i*h] = 0;
+            }
         }
+    }
+    
+    for (int prime : primes) {
+    cout << prime << endl;
     }
     
     return 0;
