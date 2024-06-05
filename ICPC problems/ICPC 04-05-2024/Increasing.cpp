01c554bcfdcd2b14292a53c8acb82299cd47335f
robnez44 <00044623@uca.edu.sv>
2024-06-05 00:00:26 -0600

    ordenando y subiendo increasing.cpp terminado

--- ICPC problems/ICPC 04-05-2024/Increasing.cpp
+++ ICPC problems/ICPC 04-05-2024/Increasing.cpp
@@ -1,27 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+const int inf = 2000000000;
+const int MAX = 100000;
+int n;
+int values[MAX + 5];
+int I[MAX + 5];
 
-    int n;
-    vector<int> lista(n);
-
-
-    cin >> n;
-
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> lista[i];
+int lis() {
+    int i, low, high, mid;
+    I[0] = -inf;
+    for (i = 1; i <= n; i++) I[i] = inf;
+    int ans = 0;
+    vector<int> seen;
+    for(i = 0; i < n; i++) {
+        if (find(seen.begin(), seen.end(), values[i]) != seen.end()) continue; // Skip duplicates
+        seen.push_back(values[i]);
+        low = mid = 0;
+        high = ans;
+        while(low <= high) {
+            mid = (low + high) / 2;
+            if(I[mid] < values[i]) low = mid + 1;
+            else high = mid - 1;
+        }
+        I[low] = values[i];
+        if(ans < low) ans = low;
     }
+    return ans;
+}
 
-    vector<int> sublista(lista);
-
-    
-
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> values[i];
+    }
+    cout << lis() << "\n";
     return 0;
-
 }
