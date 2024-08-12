a7806eb6cf3a8056fe67fbe492dfabce5a6a42cc
robnez44 <00044623@uca.edu.sv>
2024-08-12 07:23:40 -0600

    potencias de 2

---
+++ C++/potencias_de_2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+
+#define P2(n) (n == (n&-n))
+
+int main(){
+
+    for (int i = 1; i < 2000; i++)
+    {
+        if(P2(i)) cout << i << endl;
+    }
+    
+
+
+    return 0;
+}
